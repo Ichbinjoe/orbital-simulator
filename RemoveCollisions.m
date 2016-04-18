2e6fdd12831a28136c0667f94aaa0f51def91798
Joseph Hirschfeld <joe@ibj.io>
2016-04-18 16:22:17 -0400

    Set functions to new schema

--- RemoveCollisions.m
+++ RemoveCollisions.m
@@ -5,12 +5,12 @@ function [CelestialObjects] = RemoveCollisions(CelestialObjects)
 	end
 	for i = 1:length(CelestialObjects) - 1
 		for j = i:length(CelestialObjects)
-			rdus = CelestialObjects(i,5) + CelestialObjects(j, 5);
-			c_dist_x = CelestialObjects(i, 2) + CelestialObjects(j, 2);
-			c_dist_y = CelestialObjects(i, 3) + CelestialObjects(j, 3);
-			c_dist = sqrt(c_dist_x ^ 2 + c_dist_y ^ 2);
+			rdus = CelestialObjects(i,4) + CelestialObjects(j, 4);
+			c_dist_x = CelestialObjects(i, 1) + CelestialObjects(j, 1);
+			c_dist_y = CelestialObjects(i, 2) + CelestialObjects(j, 2);
+			c_dist = sqrt(c_dist_x ^ 1 + c_dist_y ^ 1);
 			if c_dist < rdus
-				if CelestialObjects(i, 5) < CelestialObjects(j, 5)
+				if CelestialObjects(i, 4) < CelestialObjects(j, 4)
 					removals = [removals i];
 				else
 					removals = [removals j];
